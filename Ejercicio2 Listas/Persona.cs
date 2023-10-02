26e521637ec8415f9e774c4cbd3904b1269eb88b
Pedro Cejudo Villar <pcejudov01@educastillalamancha.es>
2023-10-02 12:29:03 +0200

    Ejercicio 2 Listas

---
+++ Ejercicio2 Listas/Persona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_Listas
+{
+    public class Persona
+    {
+        private string nombre;
+        private int longitud;
+
+        public string Nombre // Otra manera 
+        {
+            set => nombre = value;
+            get => nombre; 
+        }
+
+        public int Longitud // Formato clasico
+        {
+            set { 
+                longitud = value; 
+             }
+
+            get { 
+                return longitud;
+             }
+        }
+
+        public Persona(String nombre)
+        {
+            this.nombre = nombre;   
+            this.longitud = nombre.Length;
+
+        }
+
+        public override string ToString()
+        {
+            return "Persona: " + nombre+" - "+ longitud;
+        }
+    }
+}
---
+++ Ejercicio2 Listas/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio2_Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Persona> listaPersonas = new List <Persona>();
+            bool cont = true;
+            string posiblePersona = "";
+
+            do
+            {
+                
+                try
+                {
+                    posiblePersona = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                if (posiblePersona.Equals("fin"))
+                {
+                    cont = false;
+                }
+                else
+                {
+                    Persona nuevaPersona = new Persona(posiblePersona);
+                    listaPersonas.Add(nuevaPersona);
+                }
+
+            } while (cont);
+
+            foreach (Persona persona in listaPersonas)
+            {
+                Console.WriteLine(persona);
+            }
+            
+        }
+    }
+}
