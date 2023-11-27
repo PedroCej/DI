e352c24537ed9d38f1cfc1807cd78f184b028bab
Pedro <pedrocejudo.v@gmail.com>
2023-11-27 17:19:00 +0100

    SQLite funcionando

--- Tema 2/SQLite/Form1.Designer.cs
+++ Tema 2/SQLite/Form1.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnConectar = new Button();
+            btnLeer = new Button();
+            btnAgregar = new Button();
+            btnActualizar = new Button();
+            btnEliminar = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            btnConectar.Location = new Point(461, 35);
+            btnConectar.Name = "btnConectar";
+            btnConectar.Size = new Size(75, 23);
+            btnConectar.TabIndex = 0;
+            btnConectar.Text = "Conectar";
+            btnConectar.TextAlign = ContentAlignment.BottomCenter;
+            btnConectar.UseVisualStyleBackColor = true;
+            btnConectar.Click += btnConectar_Click;
+            // 
+            // btnLeer
+            // 
+            btnLeer.Enabled = false;
+            btnLeer.Location = new Point(461, 64);
+            btnLeer.Name = "btnLeer";
+            btnLeer.Size = new Size(75, 23);
+            btnLeer.TabIndex = 1;
+            btnLeer.Text = "Leer";
+            btnLeer.UseVisualStyleBackColor = true;
+            btnLeer.Click += btnLeer_Click;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Enabled = false;
+            btnAgregar.Location = new Point(461, 93);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(75, 23);
+            btnAgregar.TabIndex = 2;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Enabled = false;
+            btnActualizar.Location = new Point(461, 122);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(75, 23);
+            btnActualizar.TabIndex = 3;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = true;
+            btnActualizar.Click += btnActualizar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Enabled = false;
+            btnEliminar.Location = new Point(461, 151);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(75, 23);
+            btnEliminar.TabIndex = 4;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(177, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(177, 122);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(177, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(177, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Apellidos";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(177, 193);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(359, 150);
+            dataGridView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnActualizar);
+            Controls.Add(btnAgregar);
+            Controls.Add(btnLeer);
+            Controls.Add(btnConectar);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnConectar;
+        private Button btnLeer;
+        private Button btnAgregar;
+        private Button btnActualizar;
+        private Button btnEliminar;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
     }
 }
--- Tema 2/SQLite/Form1.cs
+++ Tema 2/SQLite/Form1.cs
@@ -16,14 +16,64 @@ namespace SQLite
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
 
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
             String strAppPath = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
             String strFilePath = Path.Combine(strAppPath, "Resources");
             String strFullFilename = Path.Combine(strFilePath, "BDSQLite.db");
             conexion = new SQLiteConnection("Data Source = " + strFullFilename + " ; Version = 3; New = False; Compress = True; ");
+            conexion.Open();
+            btnLeer.Enabled = true;
+            btnAgregar.Enabled = true;
+            btnActualizar.Enabled = true;
+            btnEliminar.Enabled = true;
+        }
 
+        private void actualizarLista()
+        {
+            comando = new SQLiteCommand("SELECT * FROM Persona", conexion);
+            adaptador = new SQLiteDataAdapter(comando);
+            ds = new DataSet();
+            adaptador.Fill(ds, "Persona");
 
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+        }
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            this.actualizarLista();
+        }
 
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = textBox1.Text;
+            string apellidos = textBox2.Text;
+            comando = new SQLiteCommand($"INSERT INTO persona ('Nombre','Apellidos') VALUES ('{nombre}','{apellidos}');", conexion);
+            comando.ExecuteNonQuery();
+
+            this.actualizarLista();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string nombre = textBox1.Text;
+            string apellidos = textBox2.Text;
+            int id = dataGridView1.SelectedRows[0].Index + 1;
+            comando = new SQLiteCommand($"UPDATE Persona SET Nombre='{nombre}', Apellidos='{apellidos}' WHERE ID=" + id + "", conexion);
+            comando.ExecuteNonQuery();
+
+            this.actualizarLista();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int id = dataGridView1.SelectedRows[0].Index + 1;
+            comando = new SQLiteCommand($"DELETE FROM Persona WHERE ID=" + id + "", conexion);
+            comando.ExecuteNonQuery();
+
+            this.actualizarLista();
         }
     }
 }
